8db20305f341d591f0fc42dbd890a67e89dfeb68
Brian Pina-Estrada <pinabrian2015@gmail.com>
2019-04-04 22:20:01 -0600

    Update App.js

--- Minim/src/App.js
+++ Minim/src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from "react";
 import web3 from "./web3";
 import minim from "./Minim";
 import { Container, Card } from "semantic-ui-react";
-//music player npm package
-import ReactJkMusicPlayer from "react-jinke-music-player";
-import "react-jinke-music-player/assets/index.css";
+import MusicPlayer from "./components/MusicPlayer";
+
 
 class App extends Component {
   state = {
@@ -14,11 +13,10 @@ class App extends Component {
 
   render() {
     return (
-      <Container>
         <div>
           <h3>Minim: Blockchain Music Application</h3>
+          <MusicPlayer/>
         </div>
-      </Container>
     );
   }
 }
