e50653ca7d3e63095fea078447c9c4458cade014
AngryThundrWafl <pinabrian2015@gmail.com>
2019-04-10 00:14:54 -0600

    Working Music Player Swaps between playlist with a tab menu

--- Minim/src/App.js
+++ Minim/src/App.js
@@ -4,18 +4,43 @@ import minim from "./Minim";
 import { Container, Card } from "semantic-ui-react";
 import MusicPlayer from "./components/MusicPlayer";
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+
 
 class App extends Component {
-  state = {
-    value: "",
-    message: ""
-  };
+    constructor (props){
+        super(props);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.state = {
+            boughtSong: [false, true],
+            isAvailable: true,
+        };
+    }
+
+
+  handleSelect(){
+      this.setState({isAvailable:!this.state.isAvailable});
+  }
 
   render() {
     return (
         <div>
-          <h3>Minim: Blockchain Music Application</h3>
-          <MusicPlayer/>
+            <h3>Minim: Blockchain Music Application</h3>
+            <Tabs onSelect={this.handleSelect}>
+                <TabList>
+                    <Tab>Buy Music</Tab>
+                    <Tab>My Music</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Buy Music Panel</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>My Music Panel</h2>
+                </TabPanel>
+            </Tabs>
+          <MusicPlayer isAvailable={this.state.isAvailable} boughtSong={this.state.boughtSong}/>
         </div>
     );
   }
--- Minim/src/components/MusicPlayer.js
+++ Minim/src/components/MusicPlayer.js
@@ -2,27 +2,44 @@ import React, { Component } from "react";
 import web3 from "../web3";
 import Minim from "../Minim.js";
 import { Container, Card } from "semantic-ui-react";
+
 //music player npm package
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 
-//Options for music player react component audioList contains info of our music source, title, and song name
+//AudioLists for preview and full songs
+const previewAudioList = [
+    {
+        name: "Bordem",
+        singer: "Tyler The Creator",
+        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554430820/Minim%20Library/Cover%20Art/DEEg9ZmUIAAcdEb.jpg",
+        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436480/Minim%20Library/Music%20Preview/08_Tyler_The_Creator_-_Boredom-Preview.mp3",
+    },
+    {
+        name: "Rush Hour",
+        singer: "Mac Miller",
+        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554431255/Minim%20Library/Cover%20Art/cover.jpg",
+        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436475/Minim%20Library/Music%20Preview/03._Rush_Hour-Preview.mp3",
+    }
+];
+
+const fullAudioList = [
+    {
+        name: "Bordem",
+        singer: "Tyler The Creator",
+        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554430820/Minim%20Library/Cover%20Art/DEEg9ZmUIAAcdEb.jpg",
+        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554430881/Minim%20Library/Music/08_Tyler_The_Creator_-_Boredom.mp3",
+    },
+    {
+        name: "Rush Hour",
+        singer: "Mac Miller",
+        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554431255/Minim%20Library/Cover%20Art/cover.jpg",
+        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554431252/Minim%20Library/Music/03._Rush_Hour.mp3",
+    }
+];
+
 const options = {
     //music library preview songs only go to 20 seconds
-    audioLists: [
-        {
-            name: "Bordem",
-            singer: "Tyler The Creator",
-            cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554430820/Minim%20Library/Cover%20Art/DEEg9ZmUIAAcdEb.jpg",
-            musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436480/Minim%20Library/Music%20Preview/08_Tyler_The_Creator_-_Boredom-Preview.mp3",
-        },
-        {
-            name: "Rush Hour",
-            singer: "Mac Miller",
-            cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554431255/Minim%20Library/Cover%20Art/cover.jpg",
-            musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436475/Minim%20Library/Music%20Preview/03._Rush_Hour-Preview.mp3",
-        }
-    ],
     defaultPosition: {
         top: 300,
         left: 120
@@ -34,10 +51,36 @@ const options = {
 };
 
 export default class MusicPlayer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfSongs: 2,
+        };
+        this.buildPlaylist = this.buildPlaylist.bind(this);
+    }
+
+
+    //build the availible and owned playlist for the player
+    buildPlaylist(){
+        let playList = [];
+        //loops through songs and adds them if bought or not
+        for(let i = 0; i < 2; i++){
+            //checks if the song is bought then full song goes to available playlist
+            if(this.props.boughtSong[i] === true && this.props.isAvailable === false){
+                playList.push(fullAudioList[i]);
+            }//if song isnt bought we add song to preview playlist
+            else if(this.props.boughtSong[i] === false && this.props.isAvailable === true){
+                playList.push(previewAudioList[i]);
+            }
+        }
+        return playList;
+    }
+
 
     render() {
+        let playList = this.buildPlaylist();
         return (
-            <ReactJkMusicPlayer {...options}/>
+            <ReactJkMusicPlayer audioLists={playList} {...options}/>
         );
     }
 
