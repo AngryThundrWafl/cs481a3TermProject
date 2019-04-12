8b9f93e4c400f9a0f20286c06cdf8541143934af
Zach <zach.s.vanhandel@gmail.com>
2019-04-12 15:42:25 -0600

    Add initial working version of Contract V1

--- Minim/ethereum/contracts/Minim.sol
+++ Minim/ethereum/contracts/Minim.sol
@@ -1,93 +1,53 @@
 pragma solidity ^0.4.25;
 
 contract Minim {
-    // Data Types
 
-    // Remember: simple variables can be viewed with a CALL without having to define a getter function
+    uint songPrice;
 
-    string public name;
-    string public symbol;
+    uint numSongsRegistered;
+    string[] songsRegistered;
+    mapping(string => address) songOwners;
 
-    uint amount_for_unlock;
-    uint public memberCount;
+    mapping(address => uint) numSongsPurchased;
+    mapping(address => string[]) songsPurchased;
 
-    mapping (string => address)Trojans;     // account names
-    mapping (string => string) secrets;     // account secret messages
-    mapping (string => address[]) access;   // dynamic list of accounts that have unlocked a particular account secret
-    mapping (address => uint) balance;      // account piggy-banks.  No ether, just increments each time someone unlocks my secret.
-
-
-    //Constructor
-
-    // Set up state variables
-    constructor() public{
-        name = "Secrets";                   // not really used for anything
-        symbol = "S";                       // neither is this one
-        amount_for_unlock =1 ether;         // how much ether need to unlock a secret
-        // don't need to set memberCount, it is already zero, so don't waste gas
+    constructor() public {
+        songPrice = 0.1 ether;
+        // numSongsRegistered initialized to 0 by default
     }
 
-    //functions
-
-    // utility function to compare strings
-    function compareStrings (string a, string b) view returns (bool){
-        return keccak256(a) == keccak256(b);
+    function compareStrings(string a, string b) internal pure returns(bool) {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 
-    // Register a new Trojan account
-    function registerTrojan(string name) public {
-        // throw exception if user name is null or already registered
-        require( !compareStrings(name, "") && Trojans[name] == address(0) );
-        memberCount ++;
-        Trojans[name] = msg.sender;
+    function registerSong(string songName) public {
+        require(!compareStrings(songName, ""), "The song name must not be empty");
+        require(songOwners[songName] == address(0), "A song with that name already exists");
+        numSongsRegistered++;
+        songsRegistered.push(songName);
+        songOwners[songName] = msg.sender;
     }
 
-    // Delete a user account
-    function unregisterTrojan(string name) public {
-        // ensure that the account exists and belongs to the sender
-        require( Trojans[name] != address(0) && Trojans[name] == msg.sender );
-        Trojans[name] = address(0);
-        memberCount --;
+    function purchaseSong(string songName) public payable {
+        require(songOwners[songName] != address(0), "A song with that name does not exist");
+        require(msg.value == songPrice, "Exactly 0.1 ether is required to purchase a song");
+        songOwners[songName].transfer(msg.value);
+        songsPurchased[msg.sender].push(songName);
+        numSongsPurchased[msg.sender] += 1;
     }
 
-    // Create a secret message for an account
-    function setSecret(string name, string message) public{
-        // ensure that the account exists and belongs to the sender
-        require( Trojans[name] != address(0) && Trojans[name] == msg.sender );
-        secrets[name] = message;
-
+    function getSongsRegisteredByIndex(uint index) public view returns(string) {
+        require(index >= 0 && index < numSongsPurchased[msg.sender], "Invalid index");
+        return songsRegistered[index];
     }
 
-    // read the secret message if it is unlocked
-    function getSecret(string name) public view returns(string){
-        require( Trojans[name] != address(0) );
-        bool flag = false;  // this is a memory variable; just FYI
-
-        // check to see if user is in list of users that have unlocked the message
-        for ( uint i = 0 ; i < access[name].length ; i ++){
-            if(access[name][i] == msg.sender){
-                flag = true;
-                break;
-            }
-        }
-        if (flag ){
-            return secrets[name];
-        }
-        return "message is locked";
+    function getNumSongsPurchased() public view returns(uint) {
+        return numSongsPurchased[msg.sender];
     }
 
-
-    // pay to play:  unlock someone's message
-    function unlockMessage(string name ) public payable {
-        require( Trojans[name] != address(0) &&  msg.value == amount_for_unlock);
-        balance[Trojans[name]] += msg.value;
-        Trojans[name].transfer(msg.value);
-        access[name].push(msg.sender);
-    }
-
-    // piggy-bank to keep track of how much money I made from users that have unlocked my secret message
-    function getBalance() public view returns(uint) {
-        return balance[msg.sender];
+    function getSongsPurchasedByIndex(uint index) public view returns(string) {
+        require(index >= 0 && index < numSongsPurchased[msg.sender], "Invalid index");
+        return songsPurchased[msg.sender][index];
     }
 
 }
