a370e94bac1b7f87e050171a37b1ea0e36d0c5d6
AngryThundrWafl <pinabrian2015@gmail.com>
2019-04-11 19:21:58 -0600

    Revised Music Player.js to work with diffrent accounts

--- Minim/src/components/MusicPlayer.js
+++ Minim/src/components/MusicPlayer.js
@@ -7,36 +7,6 @@ import { Container, Card } from "semantic-ui-react";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 
-//AudioLists for preview and full songs
-const previewAudioList = [
-    {
-        name: "Bordem",
-        singer: "Tyler The Creator",
-        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554430820/Minim%20Library/Cover%20Art/DEEg9ZmUIAAcdEb.jpg",
-        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436480/Minim%20Library/Music%20Preview/08_Tyler_The_Creator_-_Boredom-Preview.mp3",
-    },
-    {
-        name: "Rush Hour",
-        singer: "Mac Miller",
-        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554431255/Minim%20Library/Cover%20Art/cover.jpg",
-        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554436475/Minim%20Library/Music%20Preview/03._Rush_Hour-Preview.mp3",
-    }
-];
-
-const fullAudioList = [
-    {
-        name: "Bordem",
-        singer: "Tyler The Creator",
-        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554430820/Minim%20Library/Cover%20Art/DEEg9ZmUIAAcdEb.jpg",
-        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554430881/Minim%20Library/Music/08_Tyler_The_Creator_-_Boredom.mp3",
-    },
-    {
-        name: "Rush Hour",
-        singer: "Mac Miller",
-        cover: "https://res.cloudinary.com/angrythundrwafl/image/upload/v1554431255/Minim%20Library/Cover%20Art/cover.jpg",
-        musicSrc: "https://res.cloudinary.com/angrythundrwafl/video/upload/v1554431252/Minim%20Library/Music/03._Rush_Hour.mp3",
-    }
-];
 
 const options = {
     //music library preview songs only go to 20 seconds
@@ -47,7 +17,8 @@ const options = {
     mode: "full",
     autoPlay: false,
     once: true,
-    defaultPlayIndex: 0,
+    showDownload: false,
+    remove: false,
 };
 
 export default class MusicPlayer extends Component {
@@ -67,10 +38,10 @@ export default class MusicPlayer extends Component {
         for(let i = 0; i < 2; i++){
             //checks if the song is bought then full song goes to available playlist
             if(this.props.boughtSong[i] === true && this.props.isAvailable === false){
-                playList.push(fullAudioList[i]);
+                playList.push(this.props.fullAudioList[i]);
             }//if song isnt bought we add song to preview playlist
             else if(this.props.boughtSong[i] === false && this.props.isAvailable === true){
-                playList.push(previewAudioList[i]);
+                playList.push(this.props.previewAudioList[i]);
             }
         }
         return playList;
@@ -79,8 +50,9 @@ export default class MusicPlayer extends Component {
 
     render() {
         let playList = this.buildPlaylist();
+        let option = options;
         return (
-            <ReactJkMusicPlayer audioLists={playList} {...options}/>
+            <ReactJkMusicPlayer audioLists={playList} {...option}/>
         );
     }
 
