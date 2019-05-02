58f391fe111cd75865055e2604996dc746c3dbad
Neil Potter <neilpotter3@gmail.com>
2019-05-02 10:31:03 -0600

    Add acknowledgements for code

--- Minim/ethereum/deploy.js
+++ Minim/ethereum/deploy.js
@@ -1,5 +1,12 @@
 // invoke the deployment with "node deploy.js" from the ethereum directory
-
+/*
+  This code was taken directly from Project 2: TrojanSecret -- web3 & React
+  Credit for this code goes to:
+    Dr. Joseph Gersch (http://www.cs.colostate.edu/~gersch/#/), the Instructor for CS481a3 Spring 2019 (http://www.cs.colostate.edu/~cs481a3/#/)
+  As well as the TAs:
+    Anurag Kumar (anurag.kumar@colostate.edu)
+    Dziugas Butkus (dziugas@rams.colostate.edu)
+ */
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 
